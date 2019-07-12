b4e17a0ed1759227f8c40e6d43618cdcad21835c
spirit-92 <spirit.92@inbox.ru>
2019-07-12 20:53:30 +0300

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url_News: 'https://newsapi.org/v2/top-headlines?country=ua&apiKey=',
+  api_key_News: '44f01e38e1714de5a4cb0cae58567372',
+  api_key_Weather:'fb0244af773b2ce6caae80d7e3385cde',
+  api_url_Weather:'https://api.openweathermap.org/data/2.5/forecast?lat=51.30&lon=31.18&'
 };
