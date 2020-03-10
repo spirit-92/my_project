1e46bb1b9d4cb8d13df249ad713bb60cc41acd05
spirit-92 <panasenkodima97@gmail.com>
2020-03-10 03:38:59 +0200

    registration add

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,21 +1,27 @@
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
-
+import {ValidationApiService} from './services/validation-api.service';
 
 @Injectable({providedIn:'root'})
 export class AuthGuard implements CanActivate {
+  auth:boolean;
   constructor(
     private router: Router,
-
+    private http:ValidationApiService
   ) {
+    http.getUser(localStorage.getItem('token')).subscribe(res =>{
+      this.auth = true
+    },error => {
+      this.auth = false
+    })
   }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    if (localStorage.getItem('token')) {
+    if (this.auth) {
       return true;
     } else {
       this.router.navigate(['/registration'], {
