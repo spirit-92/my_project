dd418b984179f0179679f11de049a772f4b0c675
spirit-92 <panasenkodima97@gmail.com>
2020-04-23 15:26:33 +0300

    new server api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   api_key_News: '44f01e38e1714de5a4cb0cae58567372',
   api_key_Weather:'fb0244af773b2ce6caae80d7e3385cde',
   api_url_Weather:'https://api.openweathermap.org/data/2.5/forecast?lat=51.30&lon=31.18&',
-  api_url_my:'https://e982734c.ngrok.io'
+  api_url_my:'https://spirit.pp.ua'
   // api_url_my:'http://localhost:1111'
   // api_url_my:'https://80bf672b.ngrok.io'
 };
