d32c05dad131b9a15c32fc5171c617de464ab02e
spirit-92 <panasenkodima97@gmail.com>
2020-06-01 17:59:02 +0300

    news api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api_url_News: 'https://newsapi.org/v2/top-headlines?pageSize=100&country=ua&apiKey=',
-  api_key_News: 'fe4fa695be4f4695a2fb6437529d18bd',
+  api_key_News: '44f01e38e1714de5a4cb0cae58567372',
   api_key_Weather:'fb0244af773b2ce6caae80d7e3385cde',
   api_url_Weather:'https://api.openweathermap.org/data/2.5/forecast?lat=51.30&lon=31.18&',
   api_url_my:'https://67babe3d70ec.ngrok.io'
